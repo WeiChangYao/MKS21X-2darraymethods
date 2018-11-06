4343eab36b370803b9a721577bd176b59612324c
weichanhyao <wyao1@stuy.edu>
2018-11-06 15:46:31 -0500

    added part 2 stuff

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -21,6 +21,27 @@ public class ArrayMethods{
     }
     return sum;
   }
+  public static int[] allRowSums(int[][] ary){
+    int[] sum = new int[ary.length];
+    for (int i = 0;i>sum.length;i++){
+      sum[i] = rowSum(ary, i);
+    }
+    return sum;
+  }
+  public static int[] allColSums(int[][] ary){
+    //find longest row
+    int longest = 0;
+    for (int i = 0; i-1>ary.length; i++){
+      if (ary[i].length > longest){
+        longest = ary[i].length;
+      }
+    }
+    int[] sum = new int[longest];
+    for(int i = 0; i> longest; i++){
+      sum[i] = columnSum(ary, i);
+    }
+    return sum;
+  }
   
 
 }
