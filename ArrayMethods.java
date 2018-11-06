a204ddb4e945af5ff08837aa5ed3d5cd077c4b78
weichanhyao <wyao1@stuy.edu>
2018-11-06 16:35:32 -0500

    fixed everything

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,6 +1,6 @@
 public class ArrayMethods{
   public static int rowSum(int[][] ary, int x){
-    if (x > ary.length){
+    if (x > ary.length-1 || x < 0){
       return 0;
     }
     int sum = 0;
@@ -10,9 +10,12 @@ public class ArrayMethods{
     return sum;
   }
   public static int columnSum(int[][] ary, int x){
+    if (x < 0){
+      return 0;
+    }
     int sum = 0;
     for (int i = 0; i<ary.length; i++){
-      if (x>ary[i].length){
+      if (x>ary[i].length-1){
         sum = sum + 0;
       }
       else{
@@ -23,7 +26,7 @@ public class ArrayMethods{
   }
   public static int[] allRowSums(int[][] ary){
     int[] sum = new int[ary.length];
-    for (int i = 0;i>sum.length;i++){
+    for (int i = 0;i<sum.length;i++){
       sum[i] = rowSum(ary, i);
     }
     return sum;
@@ -31,20 +34,20 @@ public class ArrayMethods{
   public static int[] allColSums(int[][] ary){
     //find longest row
     int longest = 0;
-    for (int i = 0; i-1>ary.length; i++){
+    for (int i = 0; i < ary.length; i++){
       if (ary[i].length > longest){
         longest = ary[i].length;
       }
     }
     int[] sum = new int[longest];
-    for(int i = 0; i> longest; i++){
+    for(int i = 0; i< longest; i++){
       sum[i] = columnSum(ary, i);
     }
     return sum;
   }
   public static boolean isRowMagic(int[][] ary){
     int m = rowSum(ary, 0);
-    for (int i = 0; i > ary.length; i++){
+    for (int i = 0; i < ary.length; i++){
       if (rowSum(ary,i) != m){
         return false;
       }
@@ -53,12 +56,12 @@ public class ArrayMethods{
   }
   public static boolean isColumnMagic(int[][] ary){
     int longest = 0;
-    for (int i = 0; i-1>ary.length; i++){
+    for (int i = 0; i<ary.length; i++){
       if (ary[i].length > longest){
         longest = ary[i].length;
       }
     }
-    for (int i = 0; i > longest; i++){
+    for (int i = 0; i < longest; i++){
       if (columnSum(ary, i) != columnSum(ary,0)){
         return false;
       }
